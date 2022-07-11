60f16e19ab5623288564fa6cf175b800aeeb0dff
Luis Francisco Roa <roa234@gmail.com>
2022-07-11 16:38:57 +0200

    feat(devtool): Add bezier editor

--- Assets/Script/Route.cs
+++ Assets/Script/Route.cs
@@ -1,28 +1,146 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Route : MonoBehaviour
+/// <summary>Bezier curve<summary>
+[Serializable]
+public class Route
 {
-    [SerializeField]
-    private Transform[] ControlPoints;
+    [SerializeReference]
+    /// <summary> List of points</summary>
+    private List<Vector3> controlPoints;
+    /// <summary>Scale for initial points relative to the center</summary>
+    private int scale = 7;
 
-    private Vector2 gizmosPosition;
-
-    private void OnDrawGizmos()
+    /// <summary>Creates a Route from a center point</summary>
+    public Route(Vector3 center)
     {
-        for (float t = 0; t <= 1; t += 0.05f)
+        controlPoints = new List<Vector3>{
+					              // punto  A
+                         center + Vector3.forward * scale,
+												// Handler del punto A en diagonal hacia arriba
+                         center + Vector3.left * scale + Vector3.forward,
+												// Handler del punto B en diagonal hacia abajo
+                        center + Vector3.right * scale + Vector3.back,
+												// point B
+                        center + Vector3.back * scale
+        };
+
+    }
+
+    public Vector3 this[int i]
+    {
+        get => controlPoints[i];
+    }
+
+    /// <summary> Number of Points </summary> 
+    public int len => controlPoints.Count;
+
+    /// <summary> Adds a segment using 3 points: 
+    ///   <list>
+    ///   a new handler for the last point
+    ///   handler of the new point (anchor) 
+    ///   the new point
+    ///   </list>
+    ///   </summary>
+    public void AddSegment(Vector3 anchor)
+    {
+        var last = controlPoints[len - 1];
+        var secondLast = controlPoints[len - 2];
+
+        // Use the last point as midpoint and the second last for the direction
+        controlPoints.Add(last * 2 - secondLast);
+        // The midpoint between the last point and the new one
+        controlPoints.Add(last + anchor * .5f);
+        // The new point 
+        controlPoints.Add(anchor);
+    }
+
+    /// <summary> Number of segments </summary>
+    public int segmentsLen
+    {
+        get
         {
-            gizmosPosition = Mathf.Pow(1 - t, 3) * ControlPoints[0].position +
-                    3 * Mathf.Pow(1 - t, 2) * t * ControlPoints[1].position +
-                    3 * (1 - t) * Mathf.Pow(t, 2) * ControlPoints[2].position +
-                    Mathf.Pow(t, 3) * ControlPoints[3].position;
-
-            Gizmos.DrawSphere(gizmosPosition, 0.25f);
+            // No se cuentan el punto final ni el ultimo incluyendo sus handlers
+            var total = len - 4;
+            // Every three points counts as a segment
+            return total / 3 + 1;
         }
+    }
 
-        Gizmos.DrawLine(new Vector2(ControlPoints[0].position.x, ControlPoints[0].position.y), new Vector2(ControlPoints[1].position.x, ControlPoints[1].position.y));
-        Gizmos.DrawLine(new Vector2(ControlPoints[2].position.x, ControlPoints[2].position.y), new Vector2(ControlPoints[3].position.x, ControlPoints[3].position.y));
+    /// <summary> Move the selected Point </summary>
+    public void movePoint(int i, Vector3 pos)
+    {
+        Vector3 deltaMove = pos - controlPoints[i];
+        controlPoints[i] = pos;
+        // Is a Handler
+        if (i % 3 == 0)
+        {
+            if (i + 1 < len)
+            {
+                controlPoints[i + 1] += deltaMove;
+            }
 
+            if (i - 1 >= 0)
+            {
+                controlPoints[i - 1] += deltaMove;
+            }
+        }
+    }
+
+    // <summary> Get current position from t value t E [0,1] </summary>
+    public Vector3 getPosition(float input)
+    {
+        /* int segmentIndex = Mathf.FloorToInt(input); */
+        /* int pointIndex = segmentIndex * 3; */
+        /* float t = input - segmentIndex; */
+        var pointIndex = 0;
+        var t = input;
+
+        float c = 1.0f - t;
+
+        float bb0 = c * c * c;
+        float bb1 = 3 * t * c * c;
+        float bb2 = 3 * t * t * c;
+        float bb3 = t * t * t;
+
+        Vector3 point = controlPoints[pointIndex + 0] * bb0 + controlPoints[pointIndex + 1] * bb1 + controlPoints[pointIndex + 2] * bb2 + controlPoints[pointIndex + 3] * bb3;
+
+
+        return point;
+    }
+
+    /// <summary>1st Derivate of bezier curve</summary>  
+    /// <returns> Speed <c>Vector3</c> at given point t E [1,0] </returns>
+    public Vector3 getTanget(float input)
+    {
+
+        /* int segmentIndex = Mathf.FloorToInt(input); */
+        /* int pointIndex = segmentIndex * 3; */
+        /* float t = input - segmentIndex; */
+        var pointIndex = 0;
+        var t = input;
+
+        Vector3 q0 = controlPoints[pointIndex] + ((controlPoints[pointIndex + 1] - controlPoints[pointIndex]) * t);
+        Vector3 q1 = controlPoints[pointIndex + 1] + ((controlPoints[pointIndex + 2] - controlPoints[pointIndex + 1]) * t);
+        Vector3 q2 = controlPoints[pointIndex + 2] + ((controlPoints[pointIndex + 3] - controlPoints[pointIndex + 2]) * t);
+
+        Vector3 r0 = q0 + ((q1 - q0) * t);
+        Vector3 r1 = q1 + ((q2 - q1) * t);
+
+        Vector3 tangent = r1 - r0;
+
+        return tangent;
+    }
+
+    public Vector3[] getSegmentFromIndex(int i)
+    {
+
+        return new Vector3[]{
+                    controlPoints[i*3 + 0],
+                    controlPoints[i*3 + 1],
+                    controlPoints[i*3 + 2],
+                    controlPoints[i*3 + 3]
+        };
     }
 }
---
+++ Assets/Script/RouteCreator.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+/// <summary> Route manager for spawning and holding the reference of <c>Routes</c> 
+/// <value>route: ref to <c>Route</c></value>
+/// <value>path: filepath for storing JSON data </value>
+/// <summary>
+[Serializable]
+public class RouteCreator : MonoBehaviour
+{
+    [SerializeField]
+    public Route route;
+    private string path;
+
+    public void CreateRoute()
+    {
+        // Load Route from or create default
+        Debug.Log("creation");
+        route = new Route(transform.position);
+    }
+
+    // Initialize the path for storing the routes
+    public void Awake()
+    {
+        path = string.Format("{0}/routes.json", Application.dataPath);
+    }
+
+    /// <summary> set the midpoint y value in all the points of the Route</summary>
+    public void alignHorizontal()
+    {
+        var sum = 0f;
+        for (int i = 0; i < route.len - 1; i++)
+        {
+            sum += route[i].y;
+        }
+
+        sum = sum / route.len;
+
+        for (int i = 0; i < route.len - 1; i++)
+        {
+            var current = route[i];
+            route.movePoint(i, new Vector3(current.x, sum, current.z));
+        }
+    }
+
+    public async void Save()
+    {
+        string data = JsonUtility.ToJson(route);
+        await File.WriteAllTextAsync(path, data);
+    }
+
+    public async Task Load()
+    {
+        if (File.Exists(path))
+        {
+            Debug.Log("Data loading");
+
+            var data = await File.ReadAllTextAsync(path);
+            route = JsonUtility.FromJson<Route>(data);
+        }
+
+    }
+
+}
