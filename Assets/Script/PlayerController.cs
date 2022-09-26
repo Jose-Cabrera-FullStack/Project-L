a61cef2a0b6d3801a8ab01503204c618c49bf27c
Jose Cabrera <murdoc.jose.6@gmail.com>
2022-09-26 11:27:48 -0300

    feat: moved player by movement inputs (A,W,D,S)

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,18 +1,27 @@
-/* using System.Collections; */
-/* using System.Collections.Generic; */
-/* using System; */
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalMove;
+    public float verticalMove;
+    public CharacterController player;
+    public Vector3 playerInput;
+    public float playerSpeed;
 
     void Start()
     {
+        player = GetComponent<CharacterController>();
+        playerSpeed = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
 
+        playerInput = Vector3.ClampMagnitude(new Vector3(horizontalMove, 0, verticalMove), 1);
+
+        player.Move(playerInput * playerSpeed * Time.deltaTime);
+    }
 }
