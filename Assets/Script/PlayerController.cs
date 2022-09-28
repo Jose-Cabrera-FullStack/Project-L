b344dba64df4908e754ca40c8b95d173d4f98e0e
Jose Cabrera <murdoc.jose.6@gmail.com>
2022-09-28 13:54:49 -0300

    feat: player can jump

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,29 +1,40 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public CharacterController controller;
+    Vector3 playerVelocity;
     public float speed = 6f;
+    public float JumpForce = 100.0f;
+    public float gravity = 9.8f;
 
     public Camera mainCamera;
     Vector3 camForward;
     Vector3 camRight;
-    public float gravity = 9.8f;
 
     void Update()
+    {
+        movement();
+    }
+
+    void movement()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        direction.y += -gravity * Time.deltaTime;
 
-        direction.y = -gravity * Time.deltaTime;
+        if(Input.GetKeyDown("space")){
+            // TODO: Needs to be improved
+            direction.y += Mathf.Sqrt(Math.Abs(JumpForce * -3.0f * gravity * Time.deltaTime));
+        }
 
         camDirection();
 
         if (direction.magnitude >= 0.1f)
         {
-            Debug.Log(camRight);
             Vector3 moveController = direction.x * camRight + direction.z * camForward;
             controller.transform.LookAt(controller.transform.position + moveController);
             controller.Move(direction * speed * Time.deltaTime);
