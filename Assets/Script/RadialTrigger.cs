2987b2a8bbce22292c31f6a72a91f86fe4051091
Jose Cabrera <murdoc.jose.6@gmail.com>
2022-11-16 11:59:02 -0300

    feat: added radial trigger

---
+++ Assets/Script/RadialTrigger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialTrigger : MonoBehaviour
+{
+    Transform player;
+
+    Vector3 playerPosition;
+    public float scalarProjection;
+    public float radius = 10f;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        playerPosition = player.transform.position;
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position, 1);
+
+        float distance = Vector3.Distance(playerPosition, transform.position);
+        Debug.Log($"{playerPosition.normalized}");
+
+        Gizmos.color = distance > radius ? Color.red : Color.blue;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+        Debug.Log($"dist:{distance}");
+
+    }
+}
