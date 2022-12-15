2de41050bf492d2e8dd9bdb2662e521fc7b7455b
Jose Cabrera <murdoc.jose.6@gmail.com>
2022-12-15 11:17:33 -0300

    feat: added AI enemy to follow the player/ before refactoring CameraManager

--- Assets/_Script/Camera/CameraManager.cs
+++ Assets/_Script/Camera/CameraManager.cs
@@ -76,3 +76,101 @@ public static class CameraManager
         }
     }
 }
+
+// TODO: Refactorizar esta clase
+
+// La función CameraManager.NextCamera y CameraManager.PrevCamera pueden fusionarse en una sola función que reciba un parámetro booleano para determinar si se debe avanzar o retroceder en la lista de cámaras.
+
+// En lugar de tener una variable cameraSelectedIndex y otra selectedCamera, se puede usar una única variable que represente el índice de la cámara seleccionada en la lista.
+
+// La función CameraManager.switchCamera no es necesaria, ya que su lógica puede incluirse directamente en las funciones CameraManager.NextCamera y CameraManager.PrevCamera.
+
+// La función CameraManager.changeLayout se puede mejorar para hacerla más legible y fácil de entender. Por ejemplo, se pueden usar nombres de variables más descriptivos y se pueden agregar comentarios que expliquen lo que hace el código.
+
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public static class CameraManager
+// {
+//     // Lista de cámaras registradas.
+//     public static List<Camera> cameras = new List<Camera>();
+
+//     // Índice de la cámara seleccionada en la lista de cámaras.
+//     static int selectedCameraIndex = 0;
+
+//     // Regístra una nueva cámara.
+//     public static void Register(Camera camera)
+//     {
+//         cameras.Add(camera);
+//     }
+
+//     // Desregistra una cámara.
+//     public static void Unregister(Camera camera)
+//     {
+//         cameras.Remove(camera);
+//     }
+
+//     // Cambia la cámara seleccionada a la siguiente o a la anterior de la lista de cámaras.
+//     public static void ChangeSelectedCamera(bool next)
+//     {
+//         // Calcula el índice de la cámara seleccionada en la lista.
+//         selectedCameraIndex = next ? 
+//             (selectedCameraIndex + 1) % cameras.Count : 
+//             (selectedCameraIndex - 1 + cameras.Count) % cameras.Count;
+
+//         // Cambia el orden de profundidad de la cámara seleccionada y las demás.
+//         cameras[selectedCameraIndex].depth = 10;
+//         for (int i = 0; i < cameras.Count; i++)
+//         {
+//             if (i != selectedCameraIndex)
+//             {
+//                 cameras[i].depth = 0;
+//             }
+//         }
+
+//         // Cambia el diseño de las cámaras.
+//         ChangeLayout();
+//     }
+
+//     // Cambia el diseño de las cámaras.
+//     static void ChangeLayout()
+//     {
+//           // Número de cámaras que no son la cámara seleccionada.
+//     int unselectedCameras = cameras.Count - 1;
+
+//     // Porcentaje de la pantalla que ocupa cada cámara no seleccionada.
+//     float split = (float)1 / unselectedCameras;
+
+//     // Recorre todas las cámaras.
+//     for (int i = 0; i < cameras.Count; i++)
+//     {
+//         // Obtiene la cámara actual.
+//         Camera camera = cameras[i];
+
+//         // Si la cámara actual no es la seleccionada...
+//         if (i != selectedCameraIndex)
+//         {
+//             // Si la cámara tenía el orden de profundidad 10...
+//             if (camera.depth == 10)
+//             {
+//                 // La coloca al comienzo del diseño.
+//                 camera.rect = new Rect(0, (float)(unselectedCameras - 1) / unselectedCameras, 0.5f, split);
+//             }
+//             else
+//             {
+//                 // De lo contrario, la divide entre el resto de cámaras no seleccionadas.
+//                 camera.rect = new Rect(0, split * i, 0.5f, split);
+//             }
+//         }
+//         // Si la cámara actual es la seleccionada...
+//         else
+//         {
+//             // La coloca en la mitad derecha de la pantalla.
+//             camera.rect = new Rect(0.5f, 0, 0.5f, 1);
+//         }
+//     }
+//     }}
+
+// Para llegar a esta conclusión, primero analicé el código y busqué formas de simplificarlo y mejorar su legibilidad. Esto implicó identificar repetidos patrones de código y buscar formas de reutilizarlos en lugar de repetirlos varias veces. También implicó buscar formas de eliminar variables y funciones innecesarias y agregar comentarios que ayuden a entender el propósito del código.
+
+// Por último, es importante tener en cuenta que este es solo un ejemplo de cómo podría mejorarse el código y que hay muchas otras formas posibles de hacerlo. La idea es buscar formas de simplificar y mejorar el código, pero siempre teniendo en cuenta las necesidades específicas de cada proyecto y de cada equipo de desarrollo.
---
+++ Assets/_Script/Enemy/Enemy.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    float deathDistance = 5f;
+    [SerializeField]
+    float distanceAway;
+    // public Transform thisObject;
+    Transform target;
+    NavMeshAgent navComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        navComponent = transform.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+
+        if (target)
+        {
+            navComponent.SetDestination(target.position);
+        }
+        else
+        {
+            if (target == null)
+            {
+                target = transform.gameObject.GetComponent<Transform>();
+            }
+            else
+            {
+                target = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+        }
+
+        // Logic to kill o subtract life
+        if (dist <= deathDistance)
+        {
+            navComponent.isStopped = true;
+        }
+        else
+        {
+            navComponent.isStopped = false;
+        }
+    }
+}
