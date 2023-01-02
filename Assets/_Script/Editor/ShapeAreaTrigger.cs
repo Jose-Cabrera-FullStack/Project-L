918d4b68bd937c6a9385243ca4d30af1990de139
Jose Cabrera <murdoc.jose.6@gmail.com>
2023-01-02 15:51:47 -0300

    fix: logic moved to its own file

---
+++ Assets/_Script/Editor/ShapeAreaTrigger.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShapeAreaTrigger))]
+public class ShapeAreaTriggerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // Get a reference to the script component
+        ShapeAreaTrigger GUIShapeAreaTrigger = (ShapeAreaTrigger)target;
+        string[] propertyToRemove = { };
+        // Draw the properties using the PropertyDrawer
+        switch (GUIShapeAreaTrigger.shape)
+        {
+            case ShapeAreaTrigger.Shape.Wedge:
+                propertyToRemove = new string[] { "radiusInner", "radiusOuter", };
+                break;
+            case ShapeAreaTrigger.Shape.Cone:
+                propertyToRemove = new string[] { "height", "radius", };
+                break;
+            case ShapeAreaTrigger.Shape.Circular:
+                propertyToRemove = new string[] { "height", "radius", "fovDeg" };
+                break;
+            default: Debug.Log("gola"); break;
+        }
+        serializedObject.Update();
+
+        DrawPropertiesExcluding(serializedObject, propertyToRemove);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
